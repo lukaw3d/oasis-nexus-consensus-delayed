70c8e61d96abb9d4c0544f58a977d16b48859f7c
lukaw3d <lukajeran@oasislabs.com>
2025-04-26 02:11:45 +0200

    don't log if nexus is decently up-to-date

--- append.mjs
+++ append.mjs
@@ -1,8 +1,13 @@
 const request_time = new Date().toISOString()
 const response = await (await fetch("https://nexus.oasis.io/v1/")).json()
-console.log(JSON.stringify({
-  ...response,
-  request_time,
-  latest_block_time_age_ms: new Date(request_time) - new Date(response.latest_block_time),
-  latest_node_block_diff: response.latest_node_block - response.latest_block,
-}))
+const latest_block_time_age_ms = new Date(request_time).getTime() - new Date(response.latest_block_time).getTime()
+const latest_node_block_diff = response.latest_node_block - response.latest_block
+
+if (latest_block_time_age_ms > 20000 || latest_node_block_diff > 3) {
+  console.log(JSON.stringify({
+    ...response,
+    request_time,
+    latest_block_time_age_ms,
+    latest_node_block_diff,
+  }))
+}
--- append2.mjs
+++ append2.mjs
@@ -1,8 +1,11 @@
 const request_time = new Date().toISOString()
 const response = await (await fetch("https://nexus.oasis.io/v1/consensus/transactions?limit=1&offset=0")).json()
 const latest_tx_time = response.transactions[0].timestamp
-console.log(JSON.stringify({
-  latest_tx_time,
-  request_time,
-  latest_tx_age_ms: new Date(request_time) - new Date(latest_tx_time),
-}))
+const latest_tx_age_ms = new Date(request_time).getTime() - new Date(latest_tx_time).getTime()
+if (latest_tx_age_ms > 20000) {
+  console.log(JSON.stringify({
+    latest_tx_time,
+    request_time,
+    latest_tx_age_ms,
+  }))
+}
